99ea7b19767e9ac33fce8c7e2623a000f38a1a67
imran zaman <imran_ujhani@yahoo.com>
2020-10-16 15:56:19 -0400

    fix branch

--- CourseTaken.cs
+++ CourseTaken.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+/*
+There are a total of n courses you have to take labelled from 0 to n - 1.
+Some courses may have prerequisites, for example, if prerequisites[i] = [ai, bi] this means you must take the course bi before the course ai.
+Given the total number of courses numCourses and a list of the prerequisite pairs, return the ordering of courses you should take to finish all courses.
+If there are many valid answers, return any of them. If it is impossible to finish all courses, return an empty array.
+ */
 namespace Amazon
 {
-
     public class CourseTaken
     {
-        private int[] _result;
-        private int resultIndex = 0;
-        public int[] FindOrder(int numCourses, int[][] prerequisites)
+        private static int[] _result;
+        private static int resultIndex = 0;
+        public static int[] FindOrder(int numCourses, int[][] prerequisites)
         {
             _result = new int[numCourses];
 
@@ -30,15 +36,18 @@ namespace Amazon
 
             for (int i = 0; i < numCourses; i++)
             {
-                if (isAdded[i]) continue;
+                
+                if (isAdded[i]) 
+                    continue;
                 var noCyclic = DFS(i, adjacencyMatrix, isVisited, isAdded);
-                if (!noCyclic) return new int[0];
+                if (!noCyclic) 
+                    return new int[0];
             }
 
             return _result;
         }
 
-        private bool DFS(int cur, HashSet<int>[] adjacencyMatrix, bool[] isVisited, bool[] isAdded)
+        private static bool DFS(int cur, HashSet<int>[] adjacencyMatrix, bool[] isVisited, bool[] isAdded)
         {
             if (isVisited[cur]) return false;
 
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Amazon")]
+[assembly: AssemblyTitle("AmazonQuestions")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Amazon")]
+[assembly: AssemblyProduct("AmazonQuestions")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c3a0a0dc-519e-4fd8-a5bf-7598735d8de4")]
+[assembly: Guid("89b1a6ef-213c-4c58-9bbf-3ef1096a0f4f")]
 
 // Version information for an assembly consists of the following four values:
 //
