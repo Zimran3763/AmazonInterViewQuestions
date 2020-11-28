e89ed5896e147de773bf39abb8fb136c8f36dd7d
imran zaman <imran_ujhani@yahoo.com>
2020-11-28 16:25:22 -0500

    Amazon Online Assessment

---
+++ AmazonOnlineAssessment/OA.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static AmazonOnlineAssessment.PrimeAirRoute;
+
+namespace AmazonOnlineAssessment
+{
+    public class OA
+    {     
+        public static void Main(string[] args)
+        {
+            int maxTravelDist = 7000;
+            List<PairInt> forwardRouteList = new List<PairInt>()
+            {
+                new PairInt(1,2000),
+                new PairInt(2,4000),
+                new PairInt(3,6000)
+
+            };
+            List<PairInt> returnRouteList = new List<PairInt>()
+            {
+                new PairInt(1,2000)
+            };
+            getOptimizedUtilize(maxTravelDist, forwardRouteList, returnRouteList);
+        }
+
+    }
+}
---
+++ AmazonOnlineAssessment/PrimeAirRoute.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmazonOnlineAssessment
+{
+    public class PrimeAirRoute
+    {
+        public class PairInt
+        {
+            public int first;
+            public int second;
+            public PairInt(int first, int second)
+            {
+                this.first = first;
+                this.second = second;
+            }
+        }
+
+        public static List<PairInt> getOptimizedUtilize(int maxTravelDist, List<PairInt> forwardRouteList, List<PairInt> returnRouteList)
+        {
+            List<PairInt> result = new List<PairInt>();
+            int max = int.MinValue;
+            for (int i = 0; i < forwardRouteList.Count; i++)
+            {
+                for (int j = 0; j < returnRouteList.Count; j++)
+                {
+
+                    int sum = forwardRouteList[i].second + returnRouteList[j].second;
+                    if (sum <= maxTravelDist)
+                    {
+                        if (sum > max)
+                        {
+                            max = sum;
+                            result = new List<PairInt>();
+                            result.Add(new PairInt(forwardRouteList[i].first, returnRouteList[j].first));
+                        }
+                        else
+                        {
+                            result.Add(new PairInt(forwardRouteList[i].first, returnRouteList[j].first));
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
